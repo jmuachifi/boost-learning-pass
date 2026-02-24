2bcfe2bbe69f369610304b892171b96664720409
Muachifi, Jodionísio <jodionisio.muachifi.external+danaher@leicabiosystems.com>
2026-02-24 15:00:51 +0000

    Delete unused images and Terraform configuration files; add new main Terraform configuration for AWS infrastructure setup.

--- terraform-004/scripts/run-localstak-azure.ps1
+++
@@ -1,7 +0,0 @@
-docker rm -f localstack-azure-alpha
-
-docker run -d --name localstack-azure-alpha `
-	-p 4567:4566 `
-	-v /var/run/docker.sock:/var/run/docker.sock `
-	-e LOCALSTACK_AUTH_TOKEN=$env:LOCALSTACK_AUTH_TOKEN `
-	localstack/localstack-azure-alpha
