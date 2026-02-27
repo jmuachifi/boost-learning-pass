3044a9dd64a3dad394a53cd16008f9de3bf0f62a
Muachifi, Jodionísio <jodionisio.muachifi.external+danaher@leicabiosystems.com>
2026-02-27 11:36:15 +0000

    feat: add Terraform configuration for LocalStack with VPC, EC2 instances, load balancer, and database

---
+++ terraform-004/dry-run-10/localstack-overrinde.tf
@@ -0,0 +1,41 @@
+# This file overrides the AWS provider configuration to
+# use LocalStack. If you wish to deploy to your own AWS
+# account, delete this file.
+
+provider "aws" {
+  access_key                  = "anaccesskey"
+  secret_key                  = "asecretkey"
+  region                      = var.aws_region
+  s3_use_path_style           = false
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "https://localhost.localstack.cloud:4566"
+    apigatewayv2   = "https://localhost.localstack.cloud:4566"
+    cloudformation = "https://localhost.localstack.cloud:4566"
+    cloudwatch     = "https://localhost.localstack.cloud:4566"
+    dynamodb       = "https://localhost.localstack.cloud:4566"
+    ec2            = "https://localhost.localstack.cloud:4566"
+    elb            = "https://localhost.localstack.cloud:4566"
+    elbv2          = "https://localhost.localstack.cloud:4566"
+    es             = "https://localhost.localstack.cloud:4566"
+    elasticache    = "https://localhost.localstack.cloud:4566"
+    firehose       = "https://localhost.localstack.cloud:4566"
+    iam            = "https://localhost.localstack.cloud:4566"
+    kinesis        = "https://localhost.localstack.cloud:4566"
+    lambda         = "https://localhost.localstack.cloud:4566"
+    rds            = "https://localhost.localstack.cloud:4566"
+    redshift       = "https://localhost.localstack.cloud:4566"
+    route53        = "https://localhost.localstack.cloud:4566"
+    s3             = "http://s3.localhost.localstack.cloud:4566"
+    secretsmanager = "https://localhost.localstack.cloud:4566"
+    ses            = "https://localhost.localstack.cloud:4566"
+    sns            = "https://localhost.localstack.cloud:4566"
+    sqs            = "https://localhost.localstack.cloud:4566"
+    ssm            = "https://localhost.localstack.cloud:4566"
+    stepfunctions  = "https://localhost.localstack.cloud:4566"
+    sts            = "https://localhost.localstack.cloud:4566"
+  }
+}
