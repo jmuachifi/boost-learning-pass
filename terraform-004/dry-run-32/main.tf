7f5a848db17e1cfb5d8ef29f5de4dd5ea1563721
Muachifi, Jodionísio <jodionisio.muachifi.external+danaher@leicabiosystems.com>
2026-03-05 14:17:19 +0000

    feat(terraform): add initial Terraform configuration for dry-run-32 with autoscaling, networking, and application setup

---
+++ terraform-004/dry-run-32/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.51.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+# A resource group is like a folder for related resources. You can delete the resource group to delete all resources in it.
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-tutorial"
+  location = "westus3"
+}
